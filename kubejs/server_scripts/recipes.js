ec25f9db3038b109ea42cd8056088d18bbd64450
Hexasphere0 <kesavgurugubelli@gmail.com>
2023-02-28 15:38:33 -0800

    ok

---
+++ kubejs/server_scripts/recipes.js
@@ -0,0 +1,16 @@
+ServerEvents.recipes(event => {
+    event.shaped(
+        "immersiveengineering:hammer",
+        [
+            " IS",
+            " DI",
+            "L  "
+        ],
+        {
+            'S': "minecraft:string",
+            "D": "minecraft:diamond",
+            "I": "minecraft:iron_ingot",
+            "L": "minecraft:stick"
+        }
+    )
+})
